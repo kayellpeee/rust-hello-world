d27692c99bc71eece77f131862a038ceb07aa895
Keenan Lidral-Porter <klp2012@gmail.com>
2015-04-25 10:27:58 -0700

    adds basic tests

--- src/main.rs
+++ src/main.rs
@@ -8,38 +8,36 @@
   * Try your function with long, random strings to make sure it handles large
   * inputs well.
   **/
-fn main() {
-    fn longest_run(run: &str) -> [usize;2] {
-        let mut result: [usize; 2] = [0, 0];
-        let mut inrun: bool = true;
-        let mut start: usize = 0;
-        let mut end: usize;
-        let mut tempstart: usize = start;
-        let mut iter = run.chars().enumerate().peekable();
+pub fn longest_run(run: &str) -> [usize;2] {
+    let mut result: [usize; 2] = [0, 0];
+    let mut inrun: bool = true;
+    let mut start: usize = 0;
+    let mut end: usize;
+    let mut tempstart: usize = start;
+    let mut iter = run.chars().enumerate().peekable();
 
-        while let Some(tuple) = iter.next() {
-            if iter.peek() != None {
-                inrun = tuple.1 == iter.peek().unwrap().1;
-            }
-            if !inrun {
-                start = tempstart;
-                end = tuple.0;
-                tempstart = tuple.0 + 1;
-                if result[1] - result[0] < end - start {
-                    result = [start, end];
-                }
+    while let Some(tuple) = iter.next() {
+        if iter.peek() != None {
+            inrun = tuple.1 == iter.peek().unwrap().1;
+        }
+        if !inrun {
+            start = tempstart;
+            end = tuple.0;
+            tempstart = tuple.0 + 1;
+            if result[1] - result[0] < end - start {
+                result = [start, end];
             }
         }
-
-        result
     }
 
-        assert_eq!(longest_run("abbbcc"), [1, 3]);
-        assert_eq!(longest_run("aabbc"), [0, 1]);
-        assert_eq!(longest_run("aabbc"), [0, 1]);
-        assert_eq!(longest_run("aabbcccc"), [4, 7]);
-        assert_eq!(longest_run("abcd"), [0, 0]);
-    #[test]
-    fn basic_cases() {
-    }
+    result
+}
+
+#[test]
+fn basic_cases() {
+    assert_eq!(longest_run("aabbc"), [0, 1]);
+    assert_eq!(longest_run("abbbcc"), [1, 3]);
+    assert_eq!(longest_run("aabbc"), [0, 1]);
+    assert_eq!(longest_run("aabbcccc"), [4, 7]);
+    assert_eq!(longest_run("abcd"), [0, 0]);
 }
